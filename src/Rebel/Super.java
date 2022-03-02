0887e9ee8e06f3fb35f98ea63948e6c4eb279bad
DELL <DELL@DESKTOP-M7BIGAP>
2022-03-02 09:38:35 +0530

    smart project

---
+++ src/Rebel/Super.java
@@ -0,0 +1,24 @@
+package Rebel;
+
+public class Super 
+{
+	public static void main(String[]args)
+	{
+	int a = 10;
+	System.out.println("in");
+
+	while(a<15)
+	{
+		
+		System.out.println("Java");
+		System.out.println("previous="+a);
+		a++;
+		System.out.println("new="+a);
+		System.out.println("out");
+	}
+	
+	}
+	
+	
+}
+	
